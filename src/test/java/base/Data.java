221d66e9d9280bb79eb042a20b59df111ac7d8bf
karim Ahmed <72787450+karim-20-maker@users.noreply.github.com>
2022-03-03 17:23:52 +0200

    Update Data.java

--- src/test/java/base/Data.java
+++ src/test/java/base/Data.java
@@ -4,7 +4,6 @@ package base;
 public class Data {
     public static final String BASEURL = "http://www.colourlovers.com/api";
     public static  String EndPoint = "/patterns";
-    public static  String numViews_Path = "patterns.pattern[0].numViews";
 
 
 
